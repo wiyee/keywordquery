670a264911113993736644da0d61440e1fc8d8f0
HYDYJ <duan_yj@126.com>
2018-10-05 16:35:40 +0800

    增加查询策略

--- src/main/query/score/Score.java
+++ src/main/query/score/Score.java
@@ -1,11 +1,17 @@
 package main.query.score;
 
+import javafx.beans.binding.DoubleExpression;
 import main.pojo.POI;
 import main.pojo.Position;
+import main.pojo.Query;
 import main.pojo.User;
+import main.query.irtree.Tree;
+import main.tools.StaticMethod;
 import main.tools.StaticValue;
+import org.json.JSONObject;
 
-import java.util.List;
+import java.text.DecimalFormat;
+import java.util.*;
 
 /**
  * Created by wiyee on 2018/4/25.
@@ -86,7 +92,7 @@ public class Score {
     private static double Cost1(List<POI> candidateList, Position position){
         double dist1 = getMaxDistBetweenUserAndPoi(candidateList,position);
         double dist2 = getMaxDistBetweenPOI(candidateList);
-        return StaticValue.beta * dist1 + (1 - StaticValue.beta) * dist2;
+        return (0.5 * dist1 + 0.5 * dist2)/1000d;
     }
 
 //==========================================preference cost============================================================
@@ -94,29 +100,104 @@ public class Score {
     /**
      * 偏好损失函数
      * @param candidateList
-     * @param user
+     * @param usrInt
      * @return
      */
-    private static double Cost2(List<POI> candidateList, User user){
-        return 0d;
+    private static double Cost2(List<POI> candidateList, Map<String,Double> usrInt, int t){
+        double result = 0d;
+        for (POI poi: candidateList){
+//            System.out.println("access");
+//            System.out.println(getAccess(poi,t));
+//            System.out.println(getPreference(poi,usrInt));
+            result = (3.0) / (getAccess(poi,t) * StaticValue.alpha + getPreference(poi,usrInt) * (1 - StaticValue.alpha));
+        }
+        return result;
     }
 
     private static double getPopular(POI poi){
-        return 0d;
+        return poi.getPopularity();
+    }
+    private static double getCrowd(POI poi, int t){
+        return poi.getCongestion()[t];
+    }
+    private static double getAccess(POI poi, int t){
+        if (poi.getAccess() != null)
+            if (poi.getAccess()[t-1] != 0.5d){
+                return poi.getAccess()[t-1];
+            }
+        return 0.5d;
     }
 
-    private static double getCrowd(POI poi){
-        return 0d;
+    /**
+     *
+     * @param poi 兴趣点对象
+     * @param userPre 用户潜在兴趣点
+     * @return
+     */
+    private static double getPreference(POI poi, Map<String,Double> userPre){
+        double result = 0d;
+        String[] cateLabel = poi.getCategoryLabel();
+        String[] topicLabel = poi.getTopicLabel();
+//        JSONObject object = new JSONObject(userPre);
+        if (cateLabel != null){
+            for (String s:cateLabel){
+                if (userPre.get(s)!=null){
+                    result += Double.parseDouble(String.valueOf(userPre.get(s)));
+                }
+            }
+        }
+
+        if (topicLabel != null){
+            for (String s:topicLabel){
+                if (userPre.get(s)!=null){
+                    result += Double.parseDouble(String.valueOf(userPre.get(s)));
+                }
+            }
+        }
+        return result * 100;
     }
 
-    private static double getPreference(POI poi, User user){
-        return 0d;
+    //==========================================cost============================================================
+    public static double getScore(List<POI> list,Query query){
+//        Map<String,Double> intMap = StaticMethod.json2Map(query.getUser().getInterestTopic());
+//        System.out.println("cost1: " + Cost1(list,query.getPosition()) * StaticValue.beta);
+//        System.out.println("cost2: " + Cost2(list,query.getUser().getTopicMap(),query.getHour()) / list.size() * (1 - StaticValue.beta));
+        return Cost1(list,query.getPosition()) * StaticValue.beta + Cost2(list,query.getUser().getTopicMap(),query.getHour()) / list.size() * (1 - StaticValue.beta);
     }
 
+    public static double getCSKQCost(List<POI> list, Query query){
+        return Cost1(list,query.getPosition());
+    }
 
     public static void main(String[] args) {
 
-        double distance = getDistBetweenCoordinate(-79.5141,43.6599,-79.3981,43.7076);
-        System.out.println("Distance is:" + distance);
+//        double distance = getDistBetweenCoordinate(120.6847393,27.8426045297,120.686075789,27.8426207568);
+//        double distance2 = getDistBetweenCoordinate(120.686075789,27.8426207568,120.6874145,27.8426598299);
+//        String result = String.format("%.3f", (distance + distance2)/1000);
+//        System.out.println("Distance is:" + result);
+        Tree tree = new Tree();
+        String userId = "-0AyZxS5C--WySnbW_Q8yQ";
+        Set<String> keySet = new HashSet<String>();
+        keySet.add("Food".toLowerCase());
+        keySet.add("eyewear".toLowerCase());
+        keySet.add("shopping".toLowerCase());
+        int hour = 12;
+        Map<String,Double> intMap = StaticMethod.json2Map(StaticValue.userInt);
+        Position position = new Position(-79.437,43.6596);
+        User user = new User(userId,"Tim",25,3.56,7,intMap);
+
+
+        Query queryTest = new Query(keySet,user,hour,position);
+        String[] poi = new String[]{"KjjQGzu0241FbeERM32dpQ", "SIJQw21uevKbwabMkX7mYQ", "lI0kHT7wPJJuT8yVFIrRdg"};
+        List<POI> list = new LinkedList<POI>();
+        for (String str: poi){
+            list.add(tree.poiMap.get(str));
+        }
+
+        double cost = Cost1(list,queryTest.getPosition()) * StaticValue.beta + Cost2(list,intMap,hour)*10/list.size()*(1 - StaticValue.beta);
+        System.out.println(cost);
+        System.out.println(Cost1(list,queryTest.getPosition()) * StaticValue.beta);
+        System.out.println(Cost2(list,intMap,hour)*10/list.size()*(1 - StaticValue.beta));
     }
+
 }
